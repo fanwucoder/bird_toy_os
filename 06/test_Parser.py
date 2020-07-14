67836a03c552f73b9644c7372039494b93851ede
fanwu <254765517@qq.com>
2020-07-14 17:55:55 +0800

    编写和添加基本测试用例

---
+++ 06/test_Parser.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+class TestParser(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_hasMoreCommands(self):
+        pass
+
+    def test_advance(self):
+        pass
+
+    def test_commandType(self):
+        pass
+
+    def test_symbol(self):
+        pass
+
+    def test_dest(self):
+        pass
+
+    def test_comp(self):
+        pass
+
+    def test_jmp(self):
+        pass
